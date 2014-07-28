6a5deb32d9301a11e917e566af7c8c857c8a9b62
power721 <power721@163.com>
2014-07-28 11:23:17 +0800

    simple crawler without threading or argpaser

---
+++ crawler.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+import urllib2
+import socket
+from bs4 import BeautifulSoup
+
+class Crawler:
+  '''
+  a Crawler for images
+  '''
+
+  def __init__(self, baseUrl):
+    self.req_header = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    self.req_timeout = 20
+    self.retry = 3
+    self.page = 1
+    self.image = 1
+    self.count = 0
+    self.maxCount = 0
+    self.baseUrl = baseUrl
+    self.pageUrl = None
+    self.imageUrl = None
+    self.imageDir = './'
+    self.soup = None
+    pass
+
+  def user_agent(self, url):
+    html = None
+    try:
+        req = urllib2.Request(url, None, self.req_header)
+        html = urllib2.urlopen(req, None, self.req_timeout)
+    except urllib2.URLError as e:
+        print 'urllib2.URLError: ', e.message
+    except socket.timeout as e:
+        if self.retry:
+          self.retry -= 1
+          self.user_agent(url)
+        else:
+          print e.message
+          return None
+
+    return html
+
+  def parse(self, url):
+    html = self.user_agent(url)
+    self.soup = BeautifulSoup(html)
+    print 'parse url: ', url
+    return self.soup
+
+
+  def save_image(self, filename):
+    content = self.user_agent(self.img).read()
+    with open(filename, 'wb') as image:
+      image.write(content)
+      self.count += 1
---
+++ mm_crawler.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+import os
+import re
+import crawler
+
+class MmCrawler(crawler.Crawler):
+  '''
+  抓取分页里面的图片列表
+  '''
+  def __init__(self, imageDir='./pics/'):
+    baseUrl = 'http://www.22mm.cc/mm/qingliang/'
+    crawler.Crawler.__init__(self, baseUrl)
+    self.imageDir = imageDir
+    if not os.path.isdir(self.imageDir):
+      os.mkdir(self.imageDir)
+      pass # 处理异常
+
+  def next_page(self):
+    if not self.pageUrl:
+      self.pageUrl = self.baseUrl
+      return self.pageUrl # 第一页
+
+    self.page += 1
+    self.pageUrl = self.baseUrl + 'index_%d.html' % self.page
+    return self.pageUrl
+
+  def image_list(self):
+    res = []
+    for li in self.soup.find('div', attrs={'class' : 'ShowPage'}).next_sibling.find_all('li'):
+      res.append('http://www.22mm.cc' + li.a.attrs['href'])
+
+    return res
+
+  def run(self):
+    while self.next_page():
+      self.parse(self.pageUrl)
+      for imageUrl in self.image_list():
+        worker = MmImageCrawler(self.baseUrl, imageUrl, self.imageDir)
+        worker.run()
+
+
+class MmImageCrawler(crawler.Crawler):
+  '''
+  抓取一个系列的图片
+  '''
+  def __init__(self, baseUrl, imageUrl, imageDir='./pics/'):
+    crawler.Crawler.__init__(self, baseUrl)
+    self.firstUrl = imageUrl
+    self.imageDir = imageDir
+    self.re = re.compile('arrayImg\[0\]="(.*?)"')
+
+  def set_url(self, imageUrl):
+    self.firstUrl = imageUrl
+
+  def next_image(self):
+    if not self.imageUrl:
+      self.imageUrl = self.firstUrl
+      return self.imageUrl
+
+    next_url = self.soup.find('div', attrs={'class' : 'ShowPage'}) # 分页
+    next_url = list(next_url.children)[2].attrs['href'] # 第二个是下一页链接
+
+    self.imageUrl = None
+    if next_url and next_url[0] != '/': # '/'开始的url是下一组图片链接
+      self.imageUrl = self.baseUrl + next_url
+      return self.imageUrl
+
+  def get_image(self):
+    img = self.re.findall(str(self.soup))[-1] # 图片地址在JavaScript代码中
+    self.img = img.replace('/big/', '/pic/')  # 真实地址需要转换
+    return self.img
+
+  def file_name(self):
+    print 'imageUrl: ', self.imageUrl
+    # 网页名字加图片后缀
+    filename = self.imageDir + self.imageUrl[self.imageUrl.rindex('/')+1:-5] + self.img[self.img.rindex('.'):]
+    filename = os.path.normpath(filename)
+    print 'filename: ', filename
+    return filename
+
+  def run(self):
+    while self.next_image():
+      self.parse(self.imageUrl)
+      self.get_image()
+      self.save_image(self.file_name())
+
+
+
+
+if __name__ == '__main__':
+  mm = MmCrawler('./pic/')
+  mm.run()
+
+  #mm.parse('http://www.22mm.cc/mm/qingliang/')
+  # #print mm.soup
+  # mm.image_list()
+  # mm.parse(mm.next_page())
+  # mm.image_list()
+
+  # baseUrl = 'http://www.22mm.cc/mm/qingliang/'
+  # imageUrl = 'http://www.22mm.cc/mm/qingliang/PiaCPPdPebCamPmai.html'
+  # worker = MmImageCrawler(baseUrl, imageUrl)
+  # worker.run()
